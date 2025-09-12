9cff006ced8b98b5c2198d2317bca4a98a135157
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-09-12 11:32:08 -0300

    Alterações na DataBase

--- Querys SQL Server/Query funcionarios.sql
+++ Querys SQL Server/Query funcionarios.sql
@@ -14,5 +14,10 @@ Nivel_Acesso INT NOT NULL
 INSERT INTO Funcionarios
 (Nome, CPF, Data_Nascimento, Email, Telefone, Endereco, Cargo, Nivel_Acesso)
 VALUES
-('Bryan Albuquerque', '52341546889', '07/04/2005', 'bryanalburquerque2@gmail.com', '11974217641', 'Rua Palmira crepaldi 50', 'Gerente', '3');
+('Bryan Souza', '52341546886', '07/04/2005', 'bryanalburquerque2@gmail.com', '11974217641', 'Rua Palmira crepaldi 50', 'Gerente', '2');
 
+UPDATE Funcionarios
+SET Nome = 'Bryan jesus'
+WHERE ID_Funcionario = 175;
+
+SELECT * FROM Funcionarios
