1604b83278a8912d0dae9283d095e309b36b9c2f
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-09-16 12:27:01 -0300

    Aplicando Design usando MVVM

--- ShadowLines/Models/Users.cs
+++ ShadowLines/Models/Users.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using Microsoft.Data.SqlClient;
 
 namespace ShadowLines.Classes
 {
     class Users
     {
-        private static string connectionString =
-               "Server=DESKTOP-BRYAN\\SQLEXPRESS;Database=ShadowLines;Trusted_Connection=True;TrustServerCertificate=true";
+        private static readonly string connectionString =
+            "Server=DESKTOP-BRYAN\\SQLEXPRESS;Database=ShadowLines;Trusted_Connection=True;TrustServerCertificate=true";
 
-        public static bool Authenticate01(string name, int id)
+        public static int Authenticate(string name, int id)
         {
             using (var conexao = new SqlConnection(connectionString))
             {
                 const string sqlAuth = @"
-                    SELECT 1
+                    SELECT Nivel_Acesso
                     FROM Funcionarios 
                     WHERE Nome = @name
-                    AND ID_Funcionario = @id
-                    AND Nivel_Acesso = 1";
+                    AND ID_Funcionario = @id";
 
                 using (var cmd = new SqlCommand(sqlAuth, conexao))
                 {
@@ -26,55 +24,10 @@ namespace ShadowLines.Classes
                     cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
 
                     conexao.Open();
-                    var ok = cmd.ExecuteScalar();
-                    return ok != null;
+                    var result = cmd.ExecuteScalar();
+                    return result == null ? 0 : Convert.ToInt32(result);
                 }
             }
-        }
-
-        public static bool Authenticate02(string name, int id)
-        {
-            using (var conexao = new SqlConnection(connectionString))
-            {
-                string sqlAuth = @"
-                    SELECT 1
-                    FROM Funcionarios 
-                    WHERE Nome = @name
-                    AND ID_Funcionario = @id
-                    AND Nivel_Acesso = 2";
-
-                using (var cmd = new SqlCommand(sqlAuth, conexao))
-                {
-                    cmd.Parameters.Add("@name", SqlDbType.VarChar, 100).Value = name;
-                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
-                    conexao.Open();
-                    var ok = cmd.ExecuteScalar();
-                    return ok != null;
-                }
-            }
-        }
-
-        public static bool Autenticate03(string name, int id)
-        {
-            using (var conexao = new SqlConnection(connectionString))
-            {
-                string sqlAuth = @"
-                    SELECT 1
-                    FROM Funcionarios 
-                    WHERE Nome = @name
-                    AND ID_Funcionario = @id
-                    AND Nivel_Acesso = 3";
-
-                using (var cmd = new SqlCommand(sqlAuth, conexao))
-                {
-                    cmd.Parameters.Add("@name", SqlDbType.VarChar, 100).Value = name;
-                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
-                    conexao.Open();
-                    var ok = cmd.ExecuteScalar();
-                    return ok != null;
-                }
-            }
-
         }
     }
 }
--- ShadowLines/Views/Login.xaml.cs
+++ ShadowLines/Views/Login.xaml.cs
@@ -1,10 +1,4 @@
-﻿using ShadowLines.Classes;
-using ShadowLines.Menus;
-using ShadowLines.ViewModels;
-using System;
-using System.DirectoryServices.ActiveDirectory;
-using System.Windows;
-
+﻿using System.Windows;
 
 namespace ShadowLines
 {
@@ -13,17 +7,6 @@ namespace ShadowLines
         public Login()
         {
             InitializeComponent();
-            DataContext = new LoginViewModel();
-        }
-
-        private void txtID_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txtNome_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-
         }
     }
 }
