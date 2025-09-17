f4359f11a2621bb869c0269ef025869d64770c77
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-09-17 13:01:01 -0300

    Alterando tecnologia

---
+++ Shadows & Lines/Forms/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Shadows___Lines
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
