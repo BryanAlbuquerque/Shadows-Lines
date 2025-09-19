cc16c3a6978b1167cc5bff9727b020890097f426
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-09-19 11:51:49 -0300

    Alteração Banco de Dados e Inicio novo Design

--- Querys SQL Server/Query funcionarios.sql
+++ Querys SQL Server/Query funcionarios.sql
@@ -14,7 +14,7 @@ Nivel_Acesso INT NOT NULL
 INSERT INTO Funcionarios
 (Nome, CPF, Data_Nascimento, Email, Telefone, Endereco, Cargo, Nivel_Acesso)
 VALUES
-('Bryan Souza', '52341546889', '07/04/2005', 'bryanalburquerque2@gmail.com', '11974217641', 'Rua Palmira crepaldi 50', 'Gerente', '3');
+('Bryan Jesus', '52341546887', '07/04/2005', 'bryanalburquerque2@gmail.com', '11974217641', 'Rua Palmira crepaldi 50', 'Gerente', '3');
 
 SELECT * FROM Funcionarios
 
