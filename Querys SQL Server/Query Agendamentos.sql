cc7451f3cd84821b369b2793765b803784cbeaba
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-09-25 12:36:55 -0300

    Alterações no Banco de Dados

--- Querys SQL Server/Query Agendamentos.sql
+++ Querys SQL Server/Query Agendamentos.sql
@@ -7,8 +7,10 @@ ClienteID INT NOT NULL,
 DataAgendamento DATETIME NOT NULL,
 Servico NVARCHAR(100),
 FuncionarioID INT,
-Status NVARCHAR(20) DEFAULT 'Agendado',
-Observacoes NVARCHAR(255),
+Valor MONEY,
+Situacao VARCHAR(30),
+Pagamento VARCHAR(50),
 )
 
 SELECT * FROM Agendamentos
+
