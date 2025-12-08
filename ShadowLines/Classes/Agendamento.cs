1eede98020d778e28bc75ca82763b552f37f9e9e
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2025-12-08 09:48:23 -0300

    Aplicando Padrão Models a Classe Agendamento

--- ShadowLines/Classes/Agendamento.cs
+++ ShadowLines/Classes/Agendamento.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using ShadowLines.Models;
 
 namespace ShadowLines.Classes
 {
@@ -9,20 +10,9 @@ namespace ShadowLines.Classes
         private readonly static string _connectionString =
             "Server=DESKTOP-BRYAN\\SQLEXPRESS;Database=ShadowLines;Trusted_Connection=True;TrustServerCertificate=true";
 
-        public int AgendamentoID { get; set; }
-        public int ClienteID { get; set; }
-        public int FuncionarioID { get; set; }
-        public DateTime DataAgendamento { get; set; }
-        public string Servico { get; set; }
-        public decimal Valor { get; set; }
-        public string Situacao { get; set; }
-        public string Pagamento { get; set; }
-        public string NomeCliente { get; set; } // Nome do cliente da tabela Clientes
-        public string NomeFuncionario { get; set; } // Nome do funcionário da tabela Funcionarios
-
-        public static List<Agendamento> Select()
+        public static List<AgendamentoModel> Select()
         {
-            var lista = new List<Agendamento>();
+            List<AgendamentoModel> lista = new List<AgendamentoModel>();
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
                 string query = @"SELECT 
@@ -46,13 +36,13 @@ namespace ShadowLines.Classes
                     {
                         while (r.Read())
                         {
-                            lista.Add(new Agendamento()
+                            lista.Add(new AgendamentoModel()
                             {
                                 AgendamentoID = r.GetInt32(r.GetOrdinal("AgendamentoID")),
                                 ClienteID = r.GetInt32(r.GetOrdinal("ClienteID")),
                                 FuncionarioID = r.GetInt32(r.GetOrdinal("FuncionarioID")),
                                 DataAgendamento = r.GetDateTime(r.GetOrdinal("DataAgendamento")),
-                                Servico = r["Servico"].ToString(),
+                                Servicos = r["Servico"].ToString(),
                                 Valor = (decimal)r["Valor"],
                                 Situacao = r["Situacao"].ToString(),
                                 Pagamento = r["Pagamento"].ToString(),
@@ -67,9 +57,9 @@ namespace ShadowLines.Classes
         }
 
 
-        public static List<Agendamento> Busca(string termo)
+        public static List<AgendamentoModel> Busca(string termo)
         {
-            var lista = new List<Agendamento>();
+           List<AgendamentoModel> lista = new List<AgendamentoModel>();
 
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
@@ -86,9 +76,9 @@ namespace ShadowLines.Classes
                     F.Nome AS NomeFuncionario
 
                     FROM Agendamentos A
-                 INNER JOIN Clientes C ON A.ClienteID = C.ClienteID
-                 INNER JOIN Funcionarios F ON A.FuncionarioID = F.FuncionarioID
-                 WHERE C.Nome_Completo LIKE '%' + @termo + '%' 
+                         INNER JOIN Clientes C ON A.ClienteID = C.ClienteID
+                         INNER JOIN Funcionarios F ON A.FuncionarioID = F.FuncionarioID
+                         WHERE C.Nome_Completo LIKE '%' + @termo + '%' 
                     OR F.Nome LIKE '%' + @termo + '%' 
                     OR A.Servico LIKE '%' + @termo + '%'
                     OR A.DataAgendamento LIKE '%' + @termo + '%'";
@@ -102,13 +92,13 @@ namespace ShadowLines.Classes
                     {
                         while (r.Read())
                         {
-                            lista.Add(new Agendamento()
+                            lista.Add(new AgendamentoModel()
                             {
                                 AgendamentoID = r.GetInt32(r.GetOrdinal("AgendamentoID")),
                                 ClienteID = r.GetInt32(r.GetOrdinal("ClienteID")),
                                 FuncionarioID = r.GetInt32(r.GetOrdinal("FuncionarioID")),
                                 DataAgendamento = r.GetDateTime(r.GetOrdinal("DataAgendamento")),
-                                Servico = r["Servico"].ToString(),
+                                Servicos = r["Servico"].ToString(),
                                 Valor = (decimal)r["Valor"],
                                 Situacao = r["Situacao"].ToString(),
                                 Pagamento = r["Pagamento"].ToString(),
@@ -119,13 +109,12 @@ namespace ShadowLines.Classes
                     }
                 }
             }
-
             return lista;
         }
 
-        public static List<Agendamento> ListaPendentes(string termo)
+        public static List<AgendamentoModel> ListaPendentes(string termo)
         {
-            var lista = new List<Agendamento>();
+            List<AgendamentoModel> lista = new List<AgendamentoModel>();
 
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
@@ -160,13 +149,13 @@ namespace ShadowLines.Classes
                     {
                         while (r.Read())
                         {
-                            lista.Add(new Agendamento()
+                            lista.Add(new AgendamentoModel()
                             {
                                 AgendamentoID = r.GetInt32(r.GetOrdinal("AgendamentoID")),
                                 ClienteID = r.GetInt32(r.GetOrdinal("ClienteID")),
                                 FuncionarioID = r.GetInt32(r.GetOrdinal("FuncionarioID")),
                                 DataAgendamento = r.GetDateTime(r.GetOrdinal("DataAgendamento")),
-                                Servico = r["Servico"].ToString(),
+                                Servicos = r["Servico"].ToString(),
                                 Valor = (decimal)r["Valor"],
                                 Situacao = r["Situacao"].ToString(),
                                 Pagamento = r["Pagamento"].ToString(),
@@ -183,7 +172,7 @@ namespace ShadowLines.Classes
 
 
 
-        public bool Insert()
+        public bool Insert(AgendamentoModel ag)
         {
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
@@ -191,16 +180,16 @@ namespace ShadowLines.Classes
                     INSERT INTO Agendamentos 
                     (ClienteID, FuncionarioID, DataAgendamento, Servico, Valor, Pagamento)
                     VALUES 
-                    (@ClienteID, @FuncionarioID, @DataAgendamento, @Servico, @Valor, @Pagamento)";
+                    (@ClienteID, @FuncionarioID, @DataAgendamento, @Servicos, @Valor, @Pagamento)";
 
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    cmd.Parameters.AddWithValue("@ClienteID", ClienteID);
-                    cmd.Parameters.AddWithValue("@FuncionarioID", FuncionarioID);
-                    cmd.Parameters.AddWithValue("@DataAgendamento", DataAgendamento);
-                    cmd.Parameters.AddWithValue("@Servico", Servico);
-                    cmd.Parameters.AddWithValue("@Valor", Valor);
-                    cmd.Parameters.AddWithValue("@Pagamento", Pagamento);
+                    cmd.Parameters.AddWithValue("@ClienteID", ag.ClienteID);
+                    cmd.Parameters.AddWithValue("@FuncionarioID", ag.FuncionarioID);
+                    cmd.Parameters.AddWithValue("@DataAgendamento", ag.DataAgendamento);
+                    cmd.Parameters.AddWithValue("@Servicos", ag.Servicos);
+                    cmd.Parameters.AddWithValue("@Valor", ag.Valor);
+                    cmd.Parameters.AddWithValue("@Pagamento", ag.Pagamento);
 
                     conn.Open();
                     return cmd.ExecuteNonQuery() > 0;
@@ -208,23 +197,23 @@ namespace ShadowLines.Classes
             }
         }
 
-        public bool Update()
+        public bool Update(AgendamentoModel ag)
         {
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
                 string query = @"
                     UPDATE Agendamentos SET
                     DataAgendamento = @DataAgendamento,
-                    Servico = @Servico,
+                    Servico = @Servicos,
                     Valor = @Valor
                     WHERE ClienteID = @ClienteID";
 
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    cmd.Parameters.AddWithValue("@ClienteID", ClienteID);
-                    cmd.Parameters.AddWithValue("@DataAgendamento", DataAgendamento);
-                    cmd.Parameters.AddWithValue("@Servico", Servico);
-                    cmd.Parameters.AddWithValue("@Valor", Valor);
+                    cmd.Parameters.AddWithValue("@ClienteID", ag.ClienteID);
+                    cmd.Parameters.AddWithValue("@DataAgendamento", ag.DataAgendamento);
+                    cmd.Parameters.AddWithValue("@Servicos", ag.Servicos);
+                    cmd.Parameters.AddWithValue("@Valor", ag.Valor);
 
                     conn.Open();
                     return cmd.ExecuteNonQuery() > 0;
@@ -233,7 +222,7 @@ namespace ShadowLines.Classes
         }
 
 
-        public bool UpdateSituacao()
+        public bool UpdateSituacao(AgendamentoModel ag)
         {
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
@@ -243,22 +232,22 @@ namespace ShadowLines.Classes
                     WHERE ClienteID = @ClienteID";
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    cmd.Parameters.AddWithValue("@ClienteID", ClienteID);
-                    cmd.Parameters.AddWithValue("@Situacao", Situacao);
+                    cmd.Parameters.AddWithValue("@ClienteID", ag.ClienteID);
+                    cmd.Parameters.AddWithValue("@Situacao", ag.Situacao);
                     conn.Open();
                     return cmd.ExecuteNonQuery() > 0;
                 }
             }
         }
 
-        public bool UpdateTable() 
+        public bool UpdateTable(AgendamentoModel ag) 
         {
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
                 string query = @"
                     UPDATE Agendamentos SET
                     FuncionarioID = @FuncionarioID,
-                    Servico = @Servico, 
+                    Servico = @Servicos, 
                     DataAgendamento = @DataAgendamento,
                     Valor = @Valor,
                     Situacao = @Situacao,
@@ -267,13 +256,13 @@ namespace ShadowLines.Classes
 
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    cmd.Parameters.AddWithValue("@ClienteID", ClienteID);
-                    cmd.Parameters.AddWithValue("@FuncionarioID", FuncionarioID);
-                    cmd.Parameters.AddWithValue("@Servico", Servico);
-                    cmd.Parameters.AddWithValue("@DataAgendamento", DataAgendamento);
-                    cmd.Parameters.AddWithValue("@Valor", Valor);
-                    cmd.Parameters.AddWithValue("@Situacao", Situacao);
-                    cmd.Parameters.AddWithValue("@Pagamento", Pagamento);
+                    cmd.Parameters.AddWithValue("@ClienteID", ag.ClienteID);
+                    cmd.Parameters.AddWithValue("@FuncionarioID", ag.FuncionarioID);
+                    cmd.Parameters.AddWithValue("@Servicos", ag.Servicos);
+                    cmd.Parameters.AddWithValue("@DataAgendamento", ag.DataAgendamento);
+                    cmd.Parameters.AddWithValue("@Valor", ag.Valor);
+                    cmd.Parameters.AddWithValue("@Situacao", ag.Situacao);
+                    cmd.Parameters.AddWithValue("@Pagamento", ag.Pagamento);
                     conn.Open();
                     return cmd.ExecuteNonQuery() > 0;
                 }
---
+++ ShadowLines/Models/AgendamentoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowLines.Models
+{
+    internal class AgendamentoModel
+    {
+        public int AgendamentoID { get; set; }
+        public int ClienteID { get; set; }
+        public int FuncionarioID { get; set; }
+        public DateTime DataAgendamento { get; set; }
+        public string Servicos { get; set; }
+        public decimal Valor { get; set; }
+        public string Situacao { get; set; }
+        public string Pagamento { get; set; }
+        public string NomeCliente { get; set; } // Nome do cliente da tabela Clientes
+        public string NomeFuncionario { get; set; } // Nome do funcionário da tabela Funcionarios
+    }
+}
